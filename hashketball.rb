d30316ffe2923b03cdad88daa007ac8c921a0a0b
keeks5456 <Akikogreen08@gmail.com>
2020-06-02 11:00:46 -0700

    done

--- hashketball.rb
+++ hashketball.rb
@@ -126,12 +126,14 @@ def game_hash
     }
   }
 end
+# def player(player_name)
 
+end
 
-
+# Build a method, num_points_scored that takes in an argument of a player's name and returns the number of points scored for that player.
 def num_points_scored(player_name)
 game_hash.each do |team_place, team_info|
-  team_info[:players].each do |player|
+  team_info[:players].each do |player| 
     if player[:player_name] == player_name
       return player[:points]
   # binding.pry
@@ -140,6 +142,7 @@ game_hash.each do |team_place, team_info|
   end
 end
 
+# Build a method, shoe_size, that takes in an argument of a player's name and returns the shoe size for that player.
 def shoe_size(player_name)
   game_hash.each do |team_place, team_info|
     team_info[:players].each do |player|
@@ -151,6 +154,7 @@ def shoe_size(player_name)
   end
 end
 
+# Build a method, team_colors, that takes in an argument of the team name and returns an Array of that team's colors.
 def team_colors(team_name)
   game_hash.each do |team_place, team_info|
     if team_info[:team_name].include? team_name
@@ -160,12 +164,14 @@ def team_colors(team_name)
   end
 end
 
+# Build a method, team_names, that operates on the game Hash to return an Array of the team names.
 def team_names
  home_away_team = [game_hash[:home][:team_name], game_hash[:away][:team_name]]
  return home_away_team
  binding.pry
 end
 
+# Build a method, player_numbers, that takes in an argument of a team name and returns an Array of the jersey numbers for that team.
 def player_numbers(team_name)
   game_hash.each do |team_place, team_info|
     if team_info[:team_name] == team_name 
@@ -173,11 +179,12 @@ def player_numbers(team_name)
     end
   end
 end
-# since team_info itereates over the keys of team_name:, colors:, & players: 
+# since team_info iterates over the keys of team_name:, colors:, & players: 
 #if we ask for the [:players] key and iterate over it with map (meaning map will touch everything in players)
 # using player to be the keys inside our :players to iterate we ask map to find the values of :number and return them in an array
 
 
+# Build a method, player_stats, that takes in an argument of a player's name and returns a hash of that player's stats.
 def player_stats(name)
   game_hash.each do |team_place, team_info|
     team_info[:players].each do |player|
@@ -191,8 +198,7 @@ def player_stats(name)
   end
 end
 
-
-
+# Build a method, big_shoe_rebounds, that will return the number of rebounds associated with the player that has the largest shoe size. Break this one down into steps:
 def big_shoe_rebounds
   large_shoe = 0 
   rebounds = 0
